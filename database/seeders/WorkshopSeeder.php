cebced9ee28764e5a0e6e1b84c7bc055f1c36b65
Nayy <nayantaka31@gmail.com>
2025-10-07 15:45:09 +0700

    workshop

--- database/seeders/WorkshopSeeder.php
+++ database/seeders/WorkshopSeeder.php
@@ -15,272 +15,272 @@ class WorkshopSeeder extends Seeder
     {
         Workshop::create([
             'name' => 'Pelatihan K3',
-            'department' => 'PRODUKSI',
+            'divisi' => 'PRODUKSI',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Penggunaan APD dan APAR',
-            'department' => 'PRODUKSI',
+            'divisi' => 'PRODUKSI',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Line Clearance',
-            'department' => 'PRODUKSI',
+            'divisi' => 'PRODUKSI',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Sanitasi & Higienitas',
-            'department' => 'PRODUKSI',
+            'divisi' => 'PRODUKSI',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Lean manufacturing dan Penerapan OEE',
-            'department' => 'PRODUKSI',
+            'divisi' => 'PRODUKSI',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Leadership',
-            'department' => 'PRODUKSI',
+            'divisi' => 'PRODUKSI',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan CPOB 2024 & Good Documentation Practice',
-            'department' => 'PRODUKSI',
+            'divisi' => 'PRODUKSI',
         ]);
 
         Workshop::create([
             'name' => 'Verifikasi Timbangan',
-            'department' => 'PRODUKSI',
+            'divisi' => 'PRODUKSI',
         ]);
 
         Workshop::create([
             'name' => 'Penggunaan Aplikasi Analisa Data',
-            'department' => 'PRODUKSI',
+            'divisi' => 'PRODUKSI',
         ]);
 
         Workshop::create([
             'name' => 'Warehouse in Pharmaceutical Industry',
-            'department' => 'SCM',
+            'divisi' => 'SCM',
         ]);
 
         Workshop::create([
             'name' => 'Leadership',
-            'department' => 'SCM',
+            'divisi' => 'SCM',
         ]);
 
         Workshop::create([
             'name' => 'Tanggap Darurat',
-            'department' => 'SCM',
+            'divisi' => 'SCM',
         ]);
 
         Workshop::create([
             'name' => 'Manual Handling K3',
-            'department' => 'SCM',
+            'divisi' => 'SCM',
         ]);
 
         Workshop::create([
             'name' => 'Penggunaan APD',
-            'department' => 'TEKNIK',
+            'divisi' => 'TEKNIK',
         ]);
 
         Workshop::create([
             'name' => '5R',
-            'department' => 'TEKNIK',
+            'divisi' => 'TEKNIK',
         ]);
 
         Workshop::create([
             'name' => 'K3 Listrik',
-            'department' => 'TEKNIK',
+            'divisi' => 'TEKNIK',
         ]);
 
         Workshop::create([
             'name' => 'Pengelasan Argon',
-            'department' => 'TEKNIK',
+            'divisi' => 'TEKNIK',
         ]);
 
         Workshop::create([
             'name' => 'Service AC',
-            'department' => 'TEKNIK',
+            'divisi' => 'TEKNIK',
         ]);
 
         Workshop::create([
             'name' => 'Service HVAC',
-            'department' => 'TEKNIK',
+            'divisi' => 'TEKNIK',
         ]);
 
         Workshop::create([
             'name' => 'Operasional Boiler K3',
-            'department' => 'TEKNIK',
+            'divisi' => 'TEKNIK',
         ]);
 
         Workshop::create([
             'name' => 'Leadership',
-            'department' => 'TEKNIK',
+            'divisi' => 'TEKNIK',
         ]);
 
         Workshop::create([
             'name' => 'CPOB Dasar',
-            'department' => 'TEKNIK',
+            'divisi' => 'TEKNIK',
         ]);
 
         Workshop::create([
             'name' => 'Good Review Practice',
-            'department' => 'RND',
+            'divisi' => 'RND',
         ]);
 
         Workshop::create([
             'name' => 'UDT dan Uji Bioekivalensi',
-            'department' => 'RND',
+            'divisi' => 'RND',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Umum',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Regulasi Pemerintah',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Teknis',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Time Management',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan AMT',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Operator Udara PPPU',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Penanggung Jawab Udara PPPU',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Operator OPL B3',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => '3Penanggung Jawab OPL B3',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'SKP dan Lisensi K3',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Ekternal - Tanggap darurat & APAR',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Ekternal - PMI',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'K3 Dasar Karyawan Baru',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Safety Riding',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Simulasi Tanggap Darurat Tumpahan',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Pengelolaan Limbah',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Kebersihan Lingkungan',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan 5R',
-            'department' => 'UMUM & SDM',
+            'divisi' => 'UMUM & SDM',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan gDocP-Laboratorium',
-            'department' => 'QC',
+            'divisi' => 'QC',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Line Clearance',
-            'department' => 'QC',
+            'divisi' => 'QC',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Good Weighing practice',
-            'department' => 'QC',
+            'divisi' => 'QC',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Problem Resolve HPLC',
-            'department' => 'QC',
+            'divisi' => 'QC',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan Problem Resolver AAS',
-            'department' => 'QC',
+            'divisi' => 'QC',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan sampling produk padat, cair dan semi solid',
-            'department' => 'QC',
+            'divisi' => 'QC',
         ]);
 
         Workshop::create([
             'name' => 'Pelatihan pengisian dokumen yang baik dan benar',
-            'department' => 'QC',
+            'divisi' => 'QC',
         ]);
 
         Workshop::create([
             'name' => 'Jobdesk',
-            'department' => 'QA',
+            'divisi' => 'QA',
         ]);
 
         Workshop::create([
             'name' => 'Refresh CPOB Dasar',
-            'department' => 'QA',
+            'divisi' => 'QA',
         ]);
 
         Workshop::create([
             'name' => 'Minitab',
-            'department' => 'QA',
+            'divisi' => 'QA',
         ]);
 
         Workshop::create([
             'name' => '2D Barcode',
-            'department' => 'QA',
+            'divisi' => 'QA',
         ]);
 
         Workshop::create([
             'name' => 'Refresh Sistem Halal',
-            'department' => 'QA',
+            'divisi' => 'QA',
         ]);
     }
 }
